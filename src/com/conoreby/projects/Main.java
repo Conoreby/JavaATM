823747d80cfb022937467fbbf27c1da9cf078f7d
Conor Eby <conoreby@gmail.com>
2017-06-07 22:18:20 -0500

    Completed program. has multiple users and gracefully handles overdraft, incorrect user ID and incorrect PIN

--- src/com/conoreby/projects/Main.java
+++ src/com/conoreby/projects/Main.java
@@ -1,17 +1,19 @@
 package com.conoreby.projects;
 
-import java.io.Console;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
 
 public class Main {
 
     private static ATM atm;
     private static boolean logoutUser;
+    private static BufferedReader bufferedReader;
 
     private static final String USER_ID_PROMPT = "Please enter your User ID to get started:";
     private static final String USER_NOT_FOUND_MESSAGE = "No user was found for the input User ID";
@@ -26,11 +28,14 @@ public class Main {
     private static final String WITHDRAWAL_PROMPT = "Please enter the amount you would like to withdraw as a dollar "
                                                 + "amount and cent amount separated by a decimal. Such as 500.00 or 10.25";
     private static final String BALANCE_RESULT_MESSAGE = "Your current balance is:\n";
+    private static final String OVERDRAFT_MESSAGE = "Transaction Cancelled: Insufficient funds";
+    private static final String INCORRECT_INPUT = "Transaction Cancelled: Incorrect Input";
 
 
     public static void main(String[] args) throws Exception {
         List<IUser> fakeUsers = createFakeUserList();
         atm = new ATM(fakeUsers);
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         logoutUser = false;
 
         startATMPromptLoop();
@@ -50,42 +55,51 @@ public class Main {
     }
 
     private static void performUserAction() throws Exception {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println(ACTION_PROMPT);
-            int choice = scanner.nextInt();
-            switch (choice) {
-                case 1: balanceAction();
-                case 2: depositAction();
-                case 3: withdrawalAction();
-                case 4: logoutAction();
-                default: break;
+        System.out.println(ACTION_PROMPT);
+        int choice = Integer.parseInt(bufferedReader.readLine());
+        switch (choice) {
+            case 1: balanceAction();
+                    break;
+            case 2: depositAction();
+                    break;
+            case 3: withdrawalAction();
+                    break;
+            case 4: logoutAction();
+            default: break;
 
-            }
         }
-
     }
 
     private static void depositAction() throws Exception {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println(DEPOSIT_PROMPT);
+        System.out.println(DEPOSIT_PROMPT);
 
-            BigDecimal depositAmount = scanner.nextBigDecimal();
+        BigDecimal depositAmount = new BigDecimal(bufferedReader.readLine());
+        try {
             atm.depositToCurrentUser(depositAmount);
-
-            //Output the new balance
-            balanceAction();
+        } catch (NegativeDepositException e) {
+            System.out.println(INCORRECT_INPUT);
+            return;
         }
+
+        //Output the new balance
+        balanceAction();
     }
 
     private static void withdrawalAction() throws Exception {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println(WITHDRAWAL_PROMPT);
+        System.out.println(WITHDRAWAL_PROMPT);
 
-            BigDecimal withdrawalAmount = scanner.nextBigDecimal();
+        BigDecimal withdrawalAmount = new BigDecimal(bufferedReader.readLine());
+        try {
             atm.withdrawFromCurrentUser(withdrawalAmount);
-
-            balanceAction();
+        } catch (OverdraftException e) {
+            System.out.println(OVERDRAFT_MESSAGE);
+            return;
+        } catch (NegativeDepositException e) {
+            System.out.println(INCORRECT_INPUT);
+            return;
         }
+
+        balanceAction();
     }
 
     private static void logoutAction() {
@@ -98,10 +112,10 @@ public class Main {
         String balanceString = NumberFormat.getCurrencyInstance(Locale.US).format(currentBalance);
 
         System.out.println(BALANCE_RESULT_MESSAGE);
-        System.out.println(balanceString);
+        System.out.println(balanceString + "\n");
     }
 
-    private static void loginUser() {
+    private static void loginUser() throws IOException {
 
         while (true) {
             long currentUserID = promptForUserID();
@@ -116,33 +130,39 @@ public class Main {
             while (true) {
                 char[] PIN = promptForUserPin();
 
+                boolean isLoggedIn = false;
                 try {
-                    atm.loginCurrentUser(PIN);
+                    isLoggedIn = atm.loginCurrentUser(PIN);
                 } catch (UserNotFoundException e) {
+                    System.out.println(USER_NOT_FOUND_MESSAGE);
+                    continue;
+                }
+
+                if (!isLoggedIn) {
                     System.out.println(INCORRECT_PIN_MESSAGE);
                     continue;
                 }
+
                 return;
             }
         }
     }
 
-    private static long promptForUserID() {
+    private static long promptForUserID() throws IOException {
         Long userID = UserManager.RESERVED_USERID;
 
-        try (Scanner scanner = new Scanner(System.in)){
 
-            while (userID == UserManager.RESERVED_USERID) {
-                System.out.println(USER_ID_PROMPT);
-                userID = scanner.nextLong();
-            }
+        while (userID == UserManager.RESERVED_USERID) {
+            System.out.println(USER_ID_PROMPT);
+            userID = Long.parseLong(bufferedReader.readLine());
         }
         return userID;
     }
 
-    private static char[] promptForUserPin() {
-        Console console = System.console();
-        return console.readPassword();
+    private static char[] promptForUserPin() throws IOException {
+            System.out.println(PIN_PROMPT);
+            String PINString = bufferedReader.readLine();
+            return PINString.toCharArray();
     }
 
     private static List<IUser> createFakeUserList() {
